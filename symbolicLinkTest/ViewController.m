db750f58b4e5384e39c6b3cc2e7096236a4afc2b
ATM <ATM@suackerdeMacBook-Pro.local>
2017-08-16 21:11:52 +0800

    first commit

--- symbolicLinkTest/ViewController.m
+++ symbolicLinkTest/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageV;
 
 @end
 
@@ -16,7 +17,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"mp4"];
+    NSString *documentPath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *uuid = nil;
+    uuid = [[NSUserDefaults standardUserDefaults] objectForKey:@"uuid"];
+    NSString *videoPath = nil;
+    if (!uuid) {
+        uuid = [[NSUUID UUID] UUIDString];
+        [[NSUserDefaults standardUserDefaults] setObject:uuid forKey:@"uuid"];
+        [[NSUserDefaults standardUserDefaults] synchronize];
+        videoPath = [documentPath stringByAppendingPathComponent:uuid];
+    }
+    //remove symbolicLink if exist
+    NSString *symbolicLinkPath = [documentPath stringByAppendingPathComponent:[uuid stringByAppendingPathExtension:@"mp4"]];
+    if ([fileManager fileExistsAtPath:symbolicLinkPath]) {
+        [fileManager removeItemAtPath:symbolicLinkPath error:nil];
+    }
+    NSLog(@"videoPath:%@",videoPath);
+    if (![fileManager fileExistsAtPath:videoPath]) {
+       [fileManager copyItemAtPath:path toPath:videoPath error:nil];
+    }
+    //create symbolicLink
+    [fileManager createSymbolicLinkAtPath:symbolicLinkPath withDestinationPath:videoPath error:nil];
+    AVURLAsset *avUrlAsset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:symbolicLinkPath] options:nil];
+    AVAssetImageGenerator *generate = [[AVAssetImageGenerator alloc] initWithAsset:avUrlAsset];
+    generate.appliesPreferredTrackTransform = YES;
+    NSError *err = NULL;
+    CMTime time = CMTimeMake(1, 60);
+    CGImageRef imgRef = [generate copyCGImageAtTime:time actualTime:NULL error:&err];
+    UIImage *image = [[UIImage alloc] initWithCGImage:imgRef];
+    self.imageV.image = image;
+    //get thumbnail image of video
+    
 }
 
 
