a1c1525c596b77f5efe6345fab14ccd369a7a91a
ATM <ATM@suackerdeMacBook-Pro.local>
2017-08-16 22:05:43 +0800

    123

--- symbolicLinkTest/ViewController.m
+++ symbolicLinkTest/ViewController.m
@@ -27,19 +27,23 @@
         uuid = [[NSUUID UUID] UUIDString];
         [[NSUserDefaults standardUserDefaults] setObject:uuid forKey:@"uuid"];
         [[NSUserDefaults standardUserDefaults] synchronize];
-        videoPath = [documentPath stringByAppendingPathComponent:uuid];
     }
+    videoPath = [documentPath stringByAppendingPathComponent:uuid];
+    if ([fileManager fileExistsAtPath:[documentPath stringByAppendingPathComponent:@"test"]]) {
+        [fileManager removeItemAtPath:[documentPath stringByAppendingPathComponent:@"test"] error:nil];
+    }
+    [fileManager createDirectoryAtPath:[documentPath stringByAppendingPathComponent:@"test"] withIntermediateDirectories:YES
+                            attributes:nil error:nil];
     //remove symbolicLink if exist
-    NSString *symbolicLinkPath = [documentPath stringByAppendingPathComponent:[uuid stringByAppendingPathExtension:@"mp4"]];
-    if ([fileManager fileExistsAtPath:symbolicLinkPath]) {
-        [fileManager removeItemAtPath:symbolicLinkPath error:nil];
-    }
+    NSString *testPath = [documentPath stringByAppendingPathComponent:@"test"];
+    NSString *symbolicLinkPath = [testPath stringByAppendingPathComponent:[uuid stringByAppendingPathExtension:@"mp4"]];
     NSLog(@"videoPath:%@",videoPath);
     if (![fileManager fileExistsAtPath:videoPath]) {
        [fileManager copyItemAtPath:path toPath:videoPath error:nil];
     }
     //create symbolicLink
     [fileManager createSymbolicLinkAtPath:symbolicLinkPath withDestinationPath:videoPath error:nil];
+    //get thumbnail image of video
     AVURLAsset *avUrlAsset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:symbolicLinkPath] options:nil];
     AVAssetImageGenerator *generate = [[AVAssetImageGenerator alloc] initWithAsset:avUrlAsset];
     generate.appliesPreferredTrackTransform = YES;
@@ -48,8 +52,6 @@
     CGImageRef imgRef = [generate copyCGImageAtTime:time actualTime:NULL error:&err];
     UIImage *image = [[UIImage alloc] initWithCGImage:imgRef];
     self.imageV.image = image;
-    //get thumbnail image of video
-    
 }
 
 
